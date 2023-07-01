1e4a4e0a7e524a6df8901ce5e73206d0e43f82ce
Divyanshu <97019230+sdivyanshu90@users.noreply.github.com>
2023-07-01 08:41:34 +0000

    Added WrongLetters.js

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Figure from './components/Figure';
+import WrongLetters from './components/WrongLetters';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    < >
+      <Header />
+      <div className='game-container'>
+        <Figure />
+        <WrongLetters />
+      </div> 
+    </>
   );
 }
 
